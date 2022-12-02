25221bbca0952ee8c81e36652a13de8c5493fdfd
DH <daniel@vendettistudios.com>
2022-12-02 13:48:08 -0800

    nav menu

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -18,7 +18,26 @@ const Nav = (props: Props) => {
                         </Image>
                     </Link>
                     <div className="flex relative top-14 w-full">
-                    <h1 className="absolute text-3xl font-extrabold left-32">Vendetti Studios</h1>
+                        <div className="flex relative top-14 w-full">
+                            <h1 className="absolute text-3xl font-extrabold left-32">Vendetti Studios</h1>
+                        </div>
+                        <div className="navMenu absolute right-10 mx-auto">
+                            <Link className="p-3"
+                                href="/About">
+                                About
+                            </Link>
+                            <Link className="pr-3"
+                                href="/Work">
+                                Work
+                            </Link>
+                            <Link className="pr-3"
+                                href="/OurTeam">
+                                Our Team
+                            </Link>
+                            <Link href="/Contact">
+                                Contact
+                            </Link>
+                        </div>
                     </div>
                 </nav>
             </header>
