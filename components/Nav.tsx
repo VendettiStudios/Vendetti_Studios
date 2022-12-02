e0ee9cfd6187422ee9bbe1bde3830d98e38e89ac
DH <daniel@vendettistudios.com>
2022-12-02 13:37:23 -0800

    added logo

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import Image from 'next/image';
+import Logo from '../public/logo.png';
+import Link from 'next/link';
 
 type Props = {}
 
 const Nav = (props: Props) => {
-  return (
-    <div>
-        <header>
-            
-        </header>
-    </div>
-  )
+    return (
+        <div>
+            <header>
+                <Link href="/">
+                    <Image
+                        className="logo top-8 left-8"
+                        src={Logo}
+                        alt="logo">
+                    </Image>
+                </Link>
+            </header>
+        </div>
+    )
 }
 
 export default Nav
